3fdc637d109ffae5ab1332b9fb5bcf8654f643a8
Daniel Rodrigues <daniel@thisisdevelopment.nl>
2021-05-31 22:36:01 +0200

    Initial commit

---
+++ configs/webpack/common.js
@@ -0,0 +1,23 @@
+// shared config (dev and prod)
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  context: resolve(__dirname, "../../src"),
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.tsx?$/],
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "index.html.ejs" })],
+  performance: {
+    hints: false,
+  },
+};
