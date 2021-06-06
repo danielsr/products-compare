927515bf684d3d1654fc85b19eb78c7325ba17b2
Daniel Rodrigues <daniel@thisisdevelopment.nl>
2021-06-06 13:14:55 +0200

    lint

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -2,5 +2,6 @@ import {} from "styled-components";
 import { Theme } from "style/theme";
 
 declare module "styled-components" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends Theme {}
 }
